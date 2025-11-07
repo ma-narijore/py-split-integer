9db8206549c3bb0c00ae179979e94faebd7405f6
Oleh <narijore@gmail.com>
2025-11-07 16:15:33 +0000

    added tests

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,23 @@
+import pytest
 from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+
+    assert 3 == sum(split_integer(3, 3))
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    assert len(set(split_integer(9, 3))) == 1
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert 3 == split_integer(3, 1)[0]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    assert split_integer(2, 4) == [0, 0, 1, 1]
